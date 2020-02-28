61bd18320a22e863b69fd2ff33c1378be322a470
sandylykova <boyola@student.42.us.org>
2020-02-27 16:47:52 -0800

    libft

---
+++ ft_striter.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/26 16:30:37 by boyola            #+#    #+#             */
+/*   Updated: 2020/02/27 16:46:27 by boyola           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed as argument.
+** Each character is passed by address to f to be modified if necessary.
+*/
+
+void	ft_striter(char *s, void (*f)(char *))
+{
+	int	i;
+
+	i = 0;
+	if (s != NULL && f != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			f(&s[i]);
+			i++;
+		}
+	}
+}
---
+++ ft_striteri.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/27 16:41:29 by boyola            #+#    #+#             */
+/*   Updated: 2020/02/27 16:47:08 by boyola           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed
+** as argument, and passing its index as first argument. Each
+** character is passed by address to f to be modified if necessary.
+*/
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	int	i;
+
+	i = 0;
+	if (s != NULL && f != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
+}
